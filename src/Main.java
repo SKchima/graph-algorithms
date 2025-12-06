581557eccc510371b40fa783dc8dec365faa1cd2
SKchima <pvmacedodias@gmail.com>
2025-12-06 20:16:01 -0300

    small changes

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,5 @@ public class Main {
 //        File file = new File(args[0]);
 //        Graph graph = Builders.undirectedGraphBuilder(file);
 
-        Digraph g = new Digraph();
-
-        // Criando vértices
-        Graph.Vertex A = g.addVertex("A");
-        Graph.Vertex B = g.addVertex("B");
-        Graph.Vertex C = g.addVertex("C");
-        Graph.Vertex D = g.addVertex("D");
-        Graph.Vertex E = g.addVertex("E");
-
-        // Adicionando arestas
-        g.addEdge(A, B);
-        g.addEdge(B, C);
-        g.addEdge(A, D);  // cruzamento
-        g.addEdge(D, E);
-        g.addEdge(C, E);  // cross edge que NÃO forma ciclo real
-
-        // Teste de hasCycles()
-        boolean hasCycle = g.hasCycles();
-        System.out.println("O grafo tem ciclo? " + hasCycle);
     }
 }
